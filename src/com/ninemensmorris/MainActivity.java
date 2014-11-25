5f0ddc8f310fddf47a434b4d8588fd466e6f412c
Hugo Nissar <nissar.hugo@gmail.com>
2014-11-25 23:12:25 +0100

    Added a onTouchListener

--- src/com/ninemensmorris/MainActivity.java
+++ src/com/ninemensmorris/MainActivity.java
@@ -4,6 +4,7 @@ import Utils.Rules;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MotionEvent;
 
 
 
@@ -19,4 +20,16 @@ public class MainActivity extends Activity {
 		setContentView(R.layout.activity_main);
 	}
 
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+		
+		int action = event.getAction();
+		switch (action) {
+		case MotionEvent.ACTION_UP:
+			Log.i(TAG, "Action up");
+			//TODO Check position and do stuff
+			break;
+		}
+		return super.onTouchEvent(event);
+	}
 }
