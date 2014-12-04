131846185779c1468f9ae575a22efccf6edac43e
Hugo Nissar <nissar.hugo@gmail.com>
2014-12-04 14:36:22 +0100

    Fixed comments and removed unnecessary code.

--- src/com/ninemensmorris/MainActivity.java
+++ src/com/ninemensmorris/MainActivity.java
@@ -18,7 +18,6 @@ import android.view.animation.Animation.AnimationListener;
 import android.view.animation.TranslateAnimation;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
 
@@ -34,7 +33,6 @@ public class MainActivity extends Activity {
 	private ArrayList<FrameLayout> higBoxAreas;
 	private ImageView selectedChecker;
 	private FrameLayout areaToMoveTo;
-	private ImageView areaToMoveFrom;
 	private HashMap<ImageView, Integer> checkerPositions;
 
 	private boolean hasSelectedChecker = false;
@@ -49,11 +47,10 @@ public class MainActivity extends Activity {
 
 		selectedChecker = null;
 		areaToMoveTo = null;
-		areaToMoveFrom = null;
 		checkerPositions = new HashMap<ImageView, Integer>();
 		playerTurn = (TextView) findViewById(R.id.TurnText);
 
-		// Initialize all views
+		//Save all white checkers in a list
 		whiteCheckers = new ArrayList<ImageView>();
 		whiteCheckers.add((ImageView) findViewById(R.id.whiteChecker1));
 		whiteCheckers.add((ImageView) findViewById(R.id.whiteChecker2));
@@ -65,6 +62,7 @@ public class MainActivity extends Activity {
 		whiteCheckers.add((ImageView) findViewById(R.id.whiteChecker8));
 		whiteCheckers.add((ImageView) findViewById(R.id.whiteChecker9));
 
+		//Save all black checkers in a list
 		blackCheckers = new ArrayList<ImageView>();
 		blackCheckers.add((ImageView) findViewById(R.id.blackChecker1));
 		blackCheckers.add((ImageView) findViewById(R.id.blackChecker2));
@@ -76,6 +74,7 @@ public class MainActivity extends Activity {
 		blackCheckers.add((ImageView) findViewById(R.id.blackChecker8));
 		blackCheckers.add((ImageView) findViewById(R.id.blackChecker9));
 
+		//Save all areas the checkers can move to in a list
 		higBoxAreas = new ArrayList<FrameLayout>();
 		higBoxAreas.add((FrameLayout) findViewById(R.id.area1));
 		higBoxAreas.add((FrameLayout) findViewById(R.id.area2));
@@ -102,6 +101,7 @@ public class MainActivity extends Activity {
 		higBoxAreas.add((FrameLayout) findViewById(R.id.area23));
 		higBoxAreas.add((FrameLayout) findViewById(R.id.area24));
 
+		//Add a onClickListener to the white checkers
 		for (ImageView v : whiteCheckers) {
 			checkerPositions.put(v, 0);
 			v.setOnClickListener(new OnClickListener() {
@@ -115,6 +115,7 @@ public class MainActivity extends Activity {
 			});
 		}
 
+		//Add a onClickListener to the black checkers
 		for (ImageView v : blackCheckers) {
 			checkerPositions.put(v, 0);
 			v.setOnClickListener(new OnClickListener() {
@@ -128,32 +129,36 @@ public class MainActivity extends Activity {
 			});
 		}
 
-		// Determines if the move is valid and changes turns
+		//add a clickListnerer to all the hit box areas
 		for (FrameLayout v : higBoxAreas) {
 			v.setOnClickListener(new OnClickListener() {
 
 				@Override
 				public void onClick(View v) {
+					//If we have a selected checker, try to move it
 					if (hasSelectedChecker) {
 						int currentTurn = rules.getTurn();
 						areaToMoveTo = (FrameLayout) v;
 
-						if (areaToMoveFrom != null) {
-							selectedChecker.setAlpha(1.0f);
-						}
+						
+						//What areas are we moving from and to?
 						int to = Integer.parseInt((String) areaToMoveTo.getContentDescription());
 						int from = checkerPositions.get(selectedChecker);
+						//Try to move the checker
 						if (rules.validMove(from, to)) { // This line will change turn
+							//Update the UI
 							unMarkAllFields();
 							moveChecker(currentTurn);
 
 							checkerPositions.put((ImageView) selectedChecker, Integer.parseInt((String) areaToMoveTo.getContentDescription()));
 
+							//Did the row create a row of 3?
 							removeNextChecker = rules.canRemove(to);
 							selectedChecker.setAlpha(1.0f);
 							hasSelectedChecker = false;
 							selectedChecker = null;
 							
+							//Update the turn text
 							if (removeNextChecker) {
 								if (currentTurn == Constants.BLACK) {
 									playerTurn.setText("Remove White");
@@ -185,6 +190,7 @@ public class MainActivity extends Activity {
 	public boolean onOptionsItemSelected(MenuItem item) {
 		// Standard menu creating
 		switch (item.getItemId()) {
+		//Start a new game
 		case R.id.item1:
 			finish();
 			startActivity(getIntent());
@@ -195,11 +201,13 @@ public class MainActivity extends Activity {
 	} 
 
 	/**
-	 * Creates an animation that will move the checker from its current position to the new. Repositions the checker after animation is done.
+	 * Move the checker from the current position to a new position
+	 * @param turn Constant.WHITE or Constant.BLACK according to whos turn it is
 	 */
 	private void moveChecker(int turn) {
-		// Create two temporary final variables so that the onAnimationEnd method uses the right values
-		ImageView animChecker = selectedChecker;
+
+		ImageView animChecker = null;
+		//Get the position of the checker that will move and the area it will move to
 		final int[] locationChecker = {0, 0};
 		final int[] locationArea = {0, 0};
 		selectedChecker.getLocationOnScreen(locationChecker);
@@ -209,30 +217,32 @@ public class MainActivity extends Activity {
 
 		ViewGroup parent = ((ViewGroup)selectedChecker.getParent());
 
+		//create a ghost checker which will be animated while the real on just moves.
 		if(turn == Constants.WHITE) {
 			animChecker = (ImageView) getLayoutInflater().inflate(R.layout.anim_white_checker, parent, false);
 		} else {
 			animChecker = (ImageView) getLayoutInflater().inflate(R.layout.anim_black_checker, parent, false);
 		}
 
-		//Move to the other layout
+		//If the checker is in the side board, we need to update the side board as well
 		if (parent != findViewById(R.id.board)) {
-			//FrameLayout placeholder = (FrameLayout) getLayoutInflater().inflate(R.layout.layout_placeholder, parent, false);
+			//Remove the real checker and add the ghost where the real one was
 			int index = parent.indexOfChild(selectedChecker);
 			parent.removeView(selectedChecker);
-			//parent.addView(placeholder, index);
-
 			parent.addView(animChecker, index);
-			//Move the checker and make it invisible
+			//Move the real one to the side board
 			((ViewGroup) findViewById(R.id.board)).addView(selectedChecker);
 		} else {
+			//Add the ghost checker at the real ones position
 			parent.addView(animChecker);
 			animChecker.setLayoutParams(selectedChecker.getLayoutParams());
 		}
 
+		//Make the real checker invisible and move it
 		selectedChecker.setLayoutParams(areaToMoveTo.getLayoutParams());
 		selectedChecker.setVisibility(View.INVISIBLE);
 
+		//final copies to be used in the animation thread
 		final ImageView tmpAnimChecker = animChecker;
 		final ImageView tmpSelectedChecker = selectedChecker;
 
@@ -247,11 +257,13 @@ public class MainActivity extends Activity {
 			@Override
 			public void onAnimationEnd(Animation animation) {
 				ViewGroup parent = ((ViewGroup)tmpAnimChecker.getParent());
+				//Fix the side board so its children stays in position
 				if (tmpAnimChecker.getParent() != findViewById(R.id.board)) {
 					FrameLayout placeholder = (FrameLayout) getLayoutInflater().inflate(R.layout.layout_placeholder, parent, false);
 					int index = parent.indexOfChild(tmpAnimChecker);
 					parent.addView(placeholder, index);		
 				}
+				//remove the ghost and make the real one visible
 				parent.removeView(tmpAnimChecker);
 				tmpSelectedChecker.setVisibility(View.VISIBLE);
 			}
@@ -269,45 +281,56 @@ public class MainActivity extends Activity {
 	}
 
 	/**
-	 * Tries to select the clicked checker. If the previous player hade 3-in-a-row, the checker will be removed instead. Also determines if someone has won.
-	 * @param v
+	 * Lets the player select a checker to remove or move
+	 * @param v The checker which was clicked on.
 	 */
 	private void selectChecker(View v) {
+		//Is it a reamove click=
 		if (removeNextChecker) {
-			// If the previous player got 3-in-a-row, remove a choosen checker from the other player			
+			//Is it a valid remove click?
 			if(rules.getTurn() == Constants.BLACK && rules.remove(checkerPositions.get(v), Constants.BLACK)) {
+				//Unamrk all options and remove the selected checker
 				unMarkAllFields();
 				blackCheckers.remove(v);
 				removeNextChecker = false;
 				ViewGroup parent = ((ViewGroup)v.getParent());
 				parent.removeView(v);
 				playerTurn.setText("Black turn");
+				
+				//Did someone win?
 				if (rules.isItAWin(Constants.BLACK)) {
 					playerTurn.setText("White wins!");
 				}
 			} 
 			else if(rules.getTurn() == Constants.WHITE && rules.remove(checkerPositions.get(v), Constants.WHITE)) {
+				//Unmark all options and remove the selected checker
 				unMarkAllFields();
 				whiteCheckers.remove(v);
 				removeNextChecker = false;
 				ViewGroup parent = ((ViewGroup)v.getParent());
 				parent.removeView(v);
 				playerTurn.setText("White turn");
+				
+				//Did someone win?
 				if (rules.isItAWin(Constants.WHITE)) {
 					playerTurn.setText("Black wins!");
 				}
 			}
-		} else if (!(checkerPositions.get(v) != 0 && checkerPositions.containsValue(0)) || (checkerPositions.get(v) == 0)) {
-			// Select checker and mark it. Remove marking from other selected checker if there is one
+		} 
+		//Try to select the checker for a move
+		else if (!(checkerPositions.get(v) != 0 && checkerPositions.containsValue(0)) || (checkerPositions.get(v) == 0)) {
+			//If a checker is already selected, unselect it
 			if (selectedChecker != null) {
 				selectedChecker.setAlpha(1.0f);
 			}
+			//If its the selected checker which is clicked, no checker is selected.
 			if(selectedChecker == v) {
 				hasSelectedChecker = false;
 				selectedChecker = null;
 				unMarkAllFields();
 				return;
 			}
+			//Select a checker and mark available moves.
 			markAvailableMoveFields(checkerPositions.get(v));
 			hasSelectedChecker = true;
 			selectedChecker = (ImageView) v;
@@ -316,8 +339,8 @@ public class MainActivity extends Activity {
 	}
 
 	/**
-	 * Mark all avalible moves that can be done.
-	 * @param from
+	 * Mark all available moves that can be done.
+	 * @param from The position of the checker which wants to move
 	 */
 	private void markAvailableMoveFields(int from) {
 		for(int i = 0; i < 24; i++) {
